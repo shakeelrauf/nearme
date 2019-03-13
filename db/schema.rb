91dfbdd7d7d51a1ad16dfafe5fe283e08b730e66
ahmedreza80 <mdahmed@shiftind.com>
2019-03-13 15:41:28 +0530

    dsjh

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190310212208) do
+ActiveRecord::Schema.define(version: 20190313095355) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20190310212208) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "shops", force: :cascade do |t|
